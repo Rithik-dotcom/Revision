1df7f66384f9f443ac1d7f9ec6415b16e2307f30
rithik singh <kushwahrithik@gmail.com>
2024-05-05 12:53:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Sectionone from './Components/Sectionone/Sectionone';
+import Footer from './Components/footer/Footer';
+import Navbarf from './Components/navbar/Navbarf';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Revision</h1>
+
+    <h3>Components</h3>
+    <Navbarf/>
+    <Footer/>
+    <Sectionone/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/navbar/Navbarf.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbarf = () => {
+  return (
+    <div>Navbarf</div>
+  )
+}
+
+export default Navbarf
